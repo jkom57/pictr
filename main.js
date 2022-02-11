48c4b1476d1a3a31a7ecd3c514beabd4b7c7f608
Joshua Komaba Chávez <2210113@etu.cegepjonquiere.ca>
2022-02-11 09:46:10 -0500

    Adding a bit of bootstrap

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ $(".albums").on("click", function () {
     let id = $(".albums").val()
     $.get(urlAlbumsId + id, function (data, status) {
         $.each(data, function (obj, item) {
-            console.log(item.thumbnailUrl);
+            //console.log(item.thumbnailUrl);
             $(".grille").append(
-                `<img src=" ${item.url} " alt="">`
+                `<img src=" ${item.url} " alt="" class="col mx-2 my-2">`
             );
         })
     })
