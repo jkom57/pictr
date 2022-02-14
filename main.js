9c7a6b21e8e2602db598323a7cea97dd339d3b94
Joshua Komaba Chávez <2210113@etu.cegepjonquiere.ca>
2022-02-13 23:51:54 -0500

    Changing to array

--- main.js
+++ main.js
@@ -3,20 +3,25 @@ urlAlbumsId = "https://jsonplaceholder.typicode.com/photos?albumId="
 
 $.get(urlAlbums, function (data, status) {
     $.each(data, function (obj, item) {
-        $(".albums").append(
+        $("#albums").append(
             `<option value="${item.id}"> ${item.title} </option>`
         )
     })
 })
 
-$(".albums").on("click", function () {
-    let id = $(".albums").val()
+$("#albums").on("click", function () {
+    let id = $("#albums").val()
     $.get(urlAlbumsId + id, function (data, status) {
+        let imgUrl = []
         $.each(data, function (obj, item) {
-            //console.log(item.thumbnailUrl);
-            $(".grille").append(
-                `<img src=" ${item.url} " alt="" class="col mx-2 my-2">`
-            );
+            imgUrl.push(item.url)
+            console.log(imgUrl)
         })
+        for (let index = 0; index < imgUrl.length; index++) {
+            console.log(imgUrl[index])
+            $("#grille").append(
+                `<img src=" ${imgUrl[index]} " alt="" class="col mx-2 my-2">`
+            );
+        }
     })
 })
