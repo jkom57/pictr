54f596484c3f5426dc871305379a0281cd696ade
Joshua Komaba Chávez <2210113@etu.cegepjonquiere.ca>
2022-02-18 09:56:41 -0500

    Lab completed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 urlAlbums =  "https://jsonplaceholder.typicode.com/albums"
 urlAlbumsId = "https://jsonplaceholder.typicode.com/photos?albumId="
+urlImagesId = "https://jsonplaceholder.typicode.com/photos/"
 
 $.get(urlAlbums, function (data, status) {
     $.each(data, function (obj, item) {
@@ -13,15 +14,45 @@ $("#albums").on("click", function () {
     let id = $("#albums").val()
     $.get(urlAlbumsId + id, function (data, status) {
         let imgUrl = []
+        let imgId = []
+        let imgTitle = []
         $.each(data, function (obj, item) {
+            imgId.push(item.id)
             imgUrl.push(item.url)
-            console.log(imgUrl)
-        })
-        for (let index = 0; index < imgUrl.length; index++) {
-            console.log(imgUrl[index])
-            $("#grille").append(
-                `<img src=" ${imgUrl[index]} " alt="" class="col mx-2 my-2">`
-            );
-        }
+            imgTitle.push(item.title)
+        });
+        albums = albumsAppended(imgId, imgUrl, imgTitle)
+        $("#grille").html(albums);
     })
+})
+
+function albumsAppended (imgId, imgUrl, imgTitle) {
+    let albums = ''
+    for (let index = 0; index < imgUrl.length; index++) {
+        albums += `<div class"card">
+        <img src="${imgUrl[index]}" alt="" class="mx-2 my-2 img-fluid image" id="${imgId[index]}">
+        <div class="card-body">
+        <center>
+        <h5 class="card-title">
+        ${imgTitle[index]}
+        </h5>
+        </center>
+        </div></div>`
+    }
+    return albums
+}
+
+$(document).on("click", "img", function () {
+    let imageId = $(this).closest(".image").attr("id")
+
+    $.get(urlImagesId + imageId, function (data, status) {
+        $(".imageModal").html(
+            `<div class="imageModal>"`+
+            `<center><h2>${data.title}</h2></center>`+
+            `<img src="${data.url}" class="img-fluid">`+
+            `</div>`
+        )
+    })
+
+    $("#modalToShow").modal('show')
 })
